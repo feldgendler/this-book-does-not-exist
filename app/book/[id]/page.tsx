83dda9e49bb2e680db54c6e9bc3b42ca4a2b688c
matangolani <matgolani@gmail.com>
2024-04-24 23:27:26 +0100

    "Adding book borders"

--- app/book/[id]/page.tsx
+++ app/book/[id]/page.tsx
@@ -36,15 +36,24 @@ export default function Book({params}: {params: {id: string}}) {
             <div className="skeleton h-4 w-full"></div>
             <div className="skeleton h-4 w-full"></div>
         </div>}
-        {title && <h1 className="mb-2">{title}</h1>}
-        {title && <aside>
-            <span className="italic">This book does not exist.</span>
-            <Link href="/" className="btn btn-xs btn-outline mx-2">Generate another</Link>
-        </aside>}
-        {overview && <p>{overview}</p>}
-        {contents && <h2 className="mb-0">Table of Contents</h2>}
-        {contents && <aside className="italic">Click on the chapters to explore</aside>}
-        {contents && <ol className="list-none">{contents.map((it) => <ContentNode key={it.key} bookID={params.id} item={it} />)}</ol>}
+        <div className="flex">
+        <div className="card card-bordered border-4 border-black w-1/2">
+            {title && <h1 className="mb-2">{title}</h1>}
+            {title && <aside>
+                <span className="italic">This book does not exist.</span>
+                <Link href="/" className="btn btn-xs btn-outline mx-2">Generate another</Link>
+            </aside>}
+            {overview && <p>{overview}</p>}
+            {contents && <h2 className="mb-0">Table of Contents</h2>}
+            {contents && <aside className="italic">Click on the chapters to explore</aside>}
+            {contents && <ol className="list-none">{contents.map((it) => <ContentNode key={it.key} bookID={params.id} item={it} />)}</ol>}
+        </div>
+        <div className="card card-bordered border-4 border-black w-1/2">
+                <p>This is the other card</p>
+        </div>
+        </div>
+        
+
     </article>;
 }
 
@@ -82,3 +91,4 @@ function ContentNode({bookID, item}: {bookID: string, item: Item}) {
         {expanded && nested && <ol className="list-none">{nested.map((it) => <ContentNode key={it.key} bookID={bookID} item={it} />)}</ol>}
     </li>;
 }
+
