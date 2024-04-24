aacd37239b0a3f5542683d779cb32590a7cfbcba
matangolani <matgolani@gmail.com>
2024-04-25 00:35:52 +0100

    "Added changed to frontend"

--- app/book/[id]/page.tsx
+++ app/book/[id]/page.tsx
@@ -28,33 +28,43 @@ export default function Book({params}: {params: {id: string}}) {
         startLoading();
     }, [params.id]);
 
-    return <article className="prose md:container md:mx-auto mt-24">
-        {!title && <div className="flex flex-col gap-4 w-96">
-            <div className="skeleton h-32 w-56"></div>
-            <div className="skeleton h-4 w-28"></div>
-            <div className="skeleton h-4 w-full"></div>
-            <div className="skeleton h-4 w-full"></div>
-            <div className="skeleton h-4 w-full"></div>
-        </div>}
-        <div className="flex">
-        <div className="card card-bordered border-4 border-black w-1/2">
-            {title && <h1 className="mb-2">{title}</h1>}
-            {title && <aside>
-                <span className="italic">This book does not exist.</span>
-                <Link href="/" className="btn btn-xs btn-outline mx-2">Hallucinate another</Link>
-            </aside>}
-            {overview && <p>{overview}</p>}
-            {contents && <h2 className="mb-0">Table of Contents</h2>}
-            {contents && <aside className="italic">Click on the chapters to explore</aside>}
-            {contents && <ol className="list-none">{contents.map((it) => <ContentNode key={it.key} bookID={params.id} item={it} />)}</ol>}
-        </div>
-        <div className="card card-bordered border-4 border-black w-1/2">
-                <p>This is the other card</p>
-        </div>
-        </div>
-        
+    return (
+        <article className="prose md:container md:mx-auto mt-24">
+            {!title && (
+                <div className="flex flex-col gap-4 w-96">
+                    <div className="skeleton h-32 w-56"></div>
+                    <div className="skeleton h-4 w-28"></div>
+                    <div className="skeleton h-4 w-full"></div>
+                    <div className="skeleton h-4 w-full"></div>
+                    <div className="skeleton h-4 w-full"></div>
+                </div>
+            )}
+            <div className="flex flex-col items-center justify-center">
+                <div className="flex mb-4">
+                    <div className="card card-bordered border-4 border-black w-1/2 aspect-w-2 aspect-h-3 flex flex-col justify-start items-center ">
+                        {title && <h1 className="mb-2 mt-4 text-center">{title}</h1>}
+                        
+                        {contents && <h2 className="mb-0 text-left">Table of Contents</h2>}
+                        {contents && <aside className="italic text-left">Click on the chapters to explore</aside>}
+                        {contents && <ol className="list-none">{contents.map((it) => <ContentNode key={it.key} bookID={params.id} item={it} />)}</ol>}
+                    </div>
+                    <div className="card card-bordered border-4 border-black w-1/2 flex flex-col items-center justify-start">
+                        {overview && <h2 className="mb-2 mt-4 text-center">Overview</h2>}
+                        {overview && <p className="mt-12 whitespace-normal max-w-md">{overview}</p>}
 
-    </article>;
+                    </div>
+                </div>
+                {title && (
+                            <aside className="flex items-center">
+                                <span className="italic">This book does not exist.</span>
+                                <Link href="/" className="btn btn-xs btn-outline mx-2">Hallucinate another</Link>
+                            </aside>
+                        )}
+        </div>
+            
+        </article>
+    );    
+
 }
 
 function ContentNode({bookID, item}: {bookID: string, item: Item}) {
